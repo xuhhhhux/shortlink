51a1f5318443b8d1811599879ace0af4c8a8b2a7
xuhhhhux <1126964021@qq.com>
2024-02-14 16:33:51 +0800

    开发短链接后管系统查询用户信息脱敏功能

--- admin/src/main/java/com/xuhh/shortlink/admin/dto/resp/UserRespDTO.java
+++ admin/src/main/java/com/xuhh/shortlink/admin/dto/resp/UserRespDTO.java
@@ -1,5 +1,7 @@
 package com.xuhh.shortlink.admin.dto.resp;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.xuhh.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
 import lombok.Data;
 
 /**
@@ -25,6 +27,7 @@ public class UserRespDTO {
     /**
      * 手机号
      */
+    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
 
     /**
