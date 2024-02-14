9d5c37fafaa38e68d4d274ae4f6bf66437819e07
xuhhhhux <1126964021@qq.com>
2024-02-14 14:09:29 +0800

    配置以及根据用户名查询用户信息

---
+++ admin/src/main/java/com/xuhh/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,13 @@
+package com.xuhh.shortlink.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.xuhh.shortlink.admin.dao.mapper")
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
---
+++ admin/src/main/java/com/xuhh/shortlink/admin/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.xuhh.shortlink.admin.controller;
+
+import com.xuhh.shortlink.admin.dto.resp.UserRespDTO;
+import com.xuhh.shortlink.admin.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户管理控制层
+ */
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 根据用户名查询用户信息
+     */
+
+    @GetMapping("/api/shortlink/v1/user/{username}")
+    public UserRespDTO getUserByUsername(@PathVariable("username") String username) {
+        return userService.getUserByUsername(username);
+    }
+}
