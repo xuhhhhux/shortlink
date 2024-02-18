1c2622a54a0d907a09a49621c87f5e158d90b096
xuhhhhux <1126964021@qq.com>
2024-02-18 10:36:53 +0800

    优化持久层实体属性继承

--- admin/src/main/java/com/xuhh/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/com/xuhh/shortlink/admin/dao/entity/UserDO.java
@@ -1,13 +1,10 @@
 package com.xuhh.shortlink.admin.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.xuhh.shortlink.admin.common.database.BaseDO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
 
 /**
  * 用户持久层实体
@@ -16,7 +13,7 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @TableName("t_user")
-public class UserDO {
+public class UserDO extends BaseDO {
     /**
      * id
      */
@@ -51,22 +48,4 @@ public class UserDO {
      * 注销时间戳
      */
     private Long deletionTime;
-
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
-
-    /**
-     * 修改时间
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
-
-    /**
-     * 删除标识 0：未删除 1：已删除
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private int delFlag;
 }
