51d671132674ce9072e40c9406644abd91e9dc33
xuhhhhux <1126964021@qq.com>
2024-02-18 10:11:42 +0800

    开发短链接后管系统短链接分组功能初始化

---
+++ admin/src/main/java/com/xuhh/shortlink/admin/service/GroupService.java
@@ -0,0 +1,10 @@
+package com.xuhh.shortlink.admin.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xuhh.shortlink.admin.dao.entity.GroupDO;
+
+/**
+ * 短链接分组接口层
+ */
+public interface GroupService extends IService<GroupDO> {
+}
